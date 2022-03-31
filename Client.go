cd194d366d1df761a96d8235ddfe0a89d8542309
tieshuaihang <tieshuaihang@beyebe.com>
2022-03-31 11:40:29 +0800

    v6 多路由支持，通过Message ID 对应到不同的处理逻辑(其实很像一个HTTP服务了)

--- Client.go
+++ Client.go
@@ -9,7 +9,7 @@ import (
 )
 
 /*
-	模拟客户端
+   模拟客户端
 */
 func main() {
 
@@ -26,7 +26,7 @@ func main() {
 	for {
 		//发封包message消息
 		dp := znet.NewDataPack()
-		msg, _ := dp.Pack(znet.NewMsgPackage(0, []byte("Zinx V0.5 Client Test Message")))
+		msg, _ := dp.Pack(znet.NewMsgPackage(0, []byte("Zinx V0.6 Client0 Test Message")))
 		_, err := conn.Write(msg)
 		if err != nil {
 			fmt.Println("write error err ", err)
--- znet/server.go
+++ znet/server.go
@@ -19,8 +19,8 @@ type Server struct {
 	IP string
 	//服务绑定的端口
 	Port int
-	//当前Server由用户绑定的回调router,也就是Server注册的链接对应的处理业务
-	Router ziface.IRouter
+	//当前Server的消息管理模块，用来绑定MsgId和对应的处理方法
+	msgHandler ziface.IMsgHandle
 }
 
 //============== 定义当前客户端链接的handle api ===========
@@ -75,7 +75,7 @@ func (s *Server) Start() {
 			//3.2 TODO Server.Start() 设置服务器最大连接控制,如果超过最大连接，那么则关闭此新的连接
 
 			//3.3 处理该新连接请求的 业务 方法， 此时应该有 handler 和 conn是绑定的
-			dealConn := NewConntion(conn, cid, s.Router)
+			dealConn := NewConntion(conn, cid, s.msgHandler)
 			cid++
 
 			//3.4 启动当前链接的处理业务
@@ -102,8 +102,8 @@ func (s *Server) Serve() {
 }
 
 //路由功能：给当前服务注册一个路由业务方法，供客户端链接处理使用
-func (s *Server) AddRouter(router ziface.IRouter) {
-	s.Router = router
+func (s *Server) AddRouter(msgId uint32, router ziface.IRouter) {
+	s.msgHandler.AddRouter(msgId, router)
 
 	fmt.Println("Add Router succ! ")
 }
@@ -116,11 +116,11 @@ func NewServer() *Server {
 	utils.GlobalObject.Reload()
 
 	s := &Server{
-		Name:      utils.GlobalObject.Name, //从全局参数获取
-		IPVersion: "tcp4",
-		IP:        utils.GlobalObject.Host,    //从全局参数获取
-		Port:      utils.GlobalObject.TcpPort, //从全局参数获取
-		Router:    nil,
+		Name:       utils.GlobalObject.Name, //从全局参数获取
+		IPVersion:  "tcp4",
+		IP:         utils.GlobalObject.Host,    //从全局参数获取
+		Port:       utils.GlobalObject.TcpPort, //从全局参数获取
+		msgHandler: NewMsgHandle(),             //msgHandler 初始
 	}
 	return s
 }

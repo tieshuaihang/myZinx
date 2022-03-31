14d7fab781096ce5888c77ca46d58c9005996875
tieshuaihang <tieshuaihang@beyebe.com>
2022-03-31 15:32:21 +0800

    v8 之前，客户端每来一条数据，就启用一个协程处理；现在，默认启动配置数量的协程，每个协程匹配一个带缓冲的channel，每次请求来，使用取模运算，将请求分配到其中一个协程的channel中，然后该协程处理此任务

--- utils/globalobj.go
+++ utils/globalobj.go
@@ -11,14 +11,27 @@ import (
 	一些参数也可以通过 用户根据 zinx.json来配置
 */
 type GlobalObj struct {
+	/*
+		Server
+	*/
 	TcpServer ziface.IServer //当前Zinx的全局Server对象
 	Host      string         //当前服务器主机IP
 	TcpPort   int            //当前服务器主机监听端口号
 	Name      string         //当前服务器名称
-	Version   string         //当前Zinx版本号
 
-	MaxPacketSize uint32 //都需数据包的最大值
-	MaxConn       int    //当前服务器主机允许的最大链接个数
+	/*
+		Zinx
+	*/
+	Version          string //当前Zinx版本号
+	MaxPacketSize    uint32 //都需数据包的最大值
+	MaxConn          int    //当前服务器主机允许的最大链接个数
+	WorkerPoolSize   uint32 //业务工作Worker池的数量
+	MaxWorkerTaskLen uint32 //业务工作Worker对应负责的任务队列最大任务存储数量
+
+	/*
+		config file path
+	*/
+	ConfFilePath string
 }
 
 /*
@@ -46,12 +59,15 @@ func (g *GlobalObj) Reload() {
 func init() {
 	//初始化GlobalObject变量，设置一些默认值
 	GlobalObject = &GlobalObj{
-		Name:          "ZinxServerApp",
-		Version:       "V0.4",
-		TcpPort:       7777,
-		Host:          "0.0.0.0",
-		MaxConn:       12000,
-		MaxPacketSize: 4096,
+		Name:             "ZinxServerApp",
+		Version:          "V0.4",
+		TcpPort:          7777,
+		Host:             "0.0.0.0",
+		MaxConn:          12000,
+		MaxPacketSize:    4096,
+		ConfFilePath:     "conf/zinx.json",
+		WorkerPoolSize:   10,
+		MaxWorkerTaskLen: 1024,
 	}
 
 	//从配置文件中加载一些用户配置的参数
